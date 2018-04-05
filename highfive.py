45a65994f81b0106861ecbe6c73c845775957a24
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-04-05 16:22:26 +0100

    added high five checker

--- highfive.py
+++ highfive.py
@@ -1,1 +1,10 @@
 from ircbot import bot
+
+@bot.hook
+def message_hook(bot, channel, sender, message):
+    if "\o/" in message:
+        bot.message(channel, "\o/")
+    elif "o/" in message:
+        bot.message(channel, "\o")
+    elif "\o" in message:
+        bot.message(channel, "o/")
