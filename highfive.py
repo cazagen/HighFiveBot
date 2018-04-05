a8a7a4b0d90eae5e18fd429dbf7ff3638449390e
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-04-05 21:14:11 +0100

    respond with ctcp action

--- highfive.py
+++ highfive.py
@@ -2,10 +2,10 @@ from ircbot import bot
 
 @bot.hook()
 def highfive_hook(bot, channel, sender, message):
-    print("test")
     if message == "\o/":
         bot.message(channel, "\o/")
     elif message == "o/":
-        bot.message(channel, "\o")
+        bot.ctcp(channel, "ACTION")
     elif message == "\o":
         bot.message(channel, "o/")
+    
