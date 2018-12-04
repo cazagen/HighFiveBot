94fa00ec4e739ef5f23ab9161b4cb33b68e94c1b
Cameron Sharp <sharpcameron@hotmail.co.uk>
2018-12-04 12:42:20 +0000

    Respond to high five anywhere in message, and respond to emoji wave

--- highfive.py
+++ highfive.py
@@ -2,11 +2,11 @@ from ircbot import bot
 
 @bot.hook()
 def highfive_hook(bot, channel, sender, message):
-    if message == "\o/":
+    if "\o/" in message:
         bot.message(channel, "\o/")
-    elif message == "o/":
-        bot.ctcp(channel, "ACTION")
-    elif message == "\o":
+    elif "o/" in message:
+        bot.ctcp(channel, "ACTION", "")
+    elif "\o" in message:
         bot.message(channel, "o/")
     elif "👋" in message:
         bot.message(channel, "o/")
